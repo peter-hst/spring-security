3dce9ef86986ba1584426964474a8d17589cce1f
peter-hst <h_st@live.cn>
2019-08-02 23:24:00 +0800

    init

---
+++ src/main/java/me/togo/security/cfg/SecurityConfig.java
@@ -0,0 +1,27 @@
+package me.togo.security.cfg;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/static/**","/js/**","/css/**","/image/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .logout().permitAll()
+                .and()
+                .formLogin();
+
+    }
+}
---
+++ src/main/java/me/togo/security/controller/HomeController.java
@@ -0,0 +1,17 @@
+package me.togo.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HomeController {
+
+    private final AtomicLong count = new AtomicLong(0);
+
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to Spring Security Demo " + count.incrementAndGet();
+    }
+}
